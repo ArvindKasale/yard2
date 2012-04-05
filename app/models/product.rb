08f5d450478b8c6f2320a980f77a4854a2d4df0e
Arvind Kasale <arvindkasale@yahoo.co.in>
2012-04-05 17:29:03 +0530

    First commit

---
+++ app/models/product.rb
@@ -0,0 +1,21 @@
+class Product < ActiveRecord::Base
+
+def self.translate
+  # query="172.20.4.112:3000/"
+  # query2="curl "+"'"+query+"'"
+  # 1000.times do
+    # system query2
+  # end
+
+  # data=Array.new
+  # data<<"Centerpoint"
+  # data<<"Footwear & Accesories"
+  # data.each do |d|
+  # query1="http://api.microsofttranslator.com/V2/Ajax.svc/Translate?appId=A5C71FCD661EB8D14BFB60D0F8A82D9615DD785C&from=en&to=ar&text=#{d}"
+  # query="curl "+"'"+query1+"'"
+#   
+  # system query
+  # end
+end   
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yard::Application.config.secret_token = 'a70d3836b00b7802a5248afa268c19e99655847154ad63a190b7e5fa9b5853c88e50e2c8ac8af965dd69fa176f7a2fd2855048298baf6d06a8699e1b227105c2'
